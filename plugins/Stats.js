dc99124d1aaf99689f07bca60071d702d8890402
Eric Glazebrook <deadlypantz@gmail.com>
2019-02-07 01:14:37 -0400

    added method apiRequest to Plugins using apiRequest(path, method, data, callback) using url via config.json file.

--- plugins/Stats.js
+++ plugins/Stats.js
@@ -8,5 +8,12 @@ export default class Stats extends Plugin {
     if (this.command === '!h') {
       this.message.channel.send('Hello');
     }
+    if (this.command === '!s') {
+      const user = this.lines[1];
+      this.apiRequest('/api/bot/stats', 'POST', `user=${user}`, (reply) => {
+        const msg = JSON.parse(reply);
+        this.message.channel.send(msg.message);
+      });
+    }
   }
 }
--- src/Plugin.js
+++ src/Plugin.js
@@ -1,3 +1,5 @@
+import Config from '../config.json';
+import http from 'https';
 export default class Plugin {
   setServices(message, command, lines) {
     this.message = message;
@@ -6,5 +8,30 @@ export default class Plugin {
   }
   run() {
     // overwrite this.
+  }
+  apiRequest(url, method, data, callback) {
+    const options = {
+      hostname: Config.url,
+      port: 443,
+      path: url,
+      method,
+      headers: {
+      },
+    };
+    if (method.toLowerCase() === 'post') {
+      options.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+      options.headers['Content-Length'] = Buffer.byteLength(data);
+    }
+    const req = http.request(options, (res) => {
+      res.setEncoding('utf-8');
+      res.on('data', (chunk) => {
+        callback(chunk);
+      });
+    });
+    req.on('error', (err) => {
+      callback(err);
+    });
+    req.write(data);
+    req.end();
   }
 }
